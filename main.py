3657c6e0a75d94dd590396949de36fcfa637da40
Cleanderson Lins <cleandersonlins@gmail.com>
2019-09-14 00:59:13 -0300

    Already getting grades

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import request_alunos_curso as rac
 # 2151 - DEPARTAMENTO DE INFORMÁTICA
 # 1858 - DEPARTAMENTO DE SISTEMAS DE COMPUTAÇÃO
 
-# courses = ufpb_courses()
+courses = ufpb_courses()
 # for each in courses:
 #     print(each)
 
@@ -21,17 +21,17 @@ import request_alunos_curso as rac
 # for student in students:
 #     print(student)
 
-students = rac.students_by_course('1626865')
-for student in students:
-    print(student)
+# students = rac.students_by_course('1626865')
+# for student in students:
+#     print(student)
 
 
-# all_students = list()
-# for each in courses:
-#     print(f"Procurando alunos do curso {each[1]}.")
-#     local = students_by_course(each[0])
-#     print(f"{len(local)} alunos encontrados.")
-#     all_students += local
+all_students = list()
+for each in courses:
+    print(f"Procurando alunos do curso {each[1]}.")
+    local = rac.students_by_course(each[0])
+    print(f"{len(local)} alunos encontrados.")
+    all_students += local
 
-# print(f"{len(all_students)} alunos encontrados em {len(courses)} cursos.")
-# print(f"A média de alunos por curso é {len(all_students)/len(courses)}")
+print(f"{len(all_students)} alunos encontrados em {len(courses)} cursos.")
+print(f"A média de alunos por curso é {len(all_students)/len(courses)}")
